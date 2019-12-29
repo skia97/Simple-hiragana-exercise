fc2b63e3aa3d05e2d3f16d32b0fa836473d5d0ec
skia97 <34816150+skia97@users.noreply.github.com>
2019-12-29 16:42:10 +0100

    Update funz.C

--- funz.C
+++ funz.C
@@ -2,6 +2,8 @@
 
 int numsing (){
     int a;
-    a=(float)rand()/RAND_MAX*107;
+    a=(float)rand()/RAND_MAX*109;
     return a;
 }
+//probabilita' sballata per ultima casistica (causa troncamento decimale), sistemato aggiungendo e 
+//spostando di uno e sommando un caso possibile, (109 e' molto meno probabile come risultato possibile)
